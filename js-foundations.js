5b4d32113db3b228571d83663dd357436ccc0c59
Ubuntu <ubuntu@ip-172-31-26-54.us-east-2.compute.internal>
2019-08-09 23:22:07 +0000

    initial commit

--- js-foundations.js
+++ js-foundations.js
@@ -2,10 +2,27 @@
 // Coding practical questions
 
 // 1. Write an arrow function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
-
-
+let divide = (num) => {
+    if(num % 3 ===0){
+        return num + " is divisible by 3"
+    } else {
+        return num + ' is not divisible by 3'
+    }
+}
+console.log(divide(15))
+console.log(divide(0))
+console.log(divide(385))
+console.log(divide(-7))
 
 // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
+var helloMe = {
+    firstName: 'Conor',
+    lastName: 'Preston', 
+    height: '6.0', 
+    eyes: 'blue',
+    getData: function(){return this.firstName + ' ' + this.lastName + ' is ' + this.height + ' foot tall and has ' + this.eyes + ' eyes'}
+}
+console.log(helloMe.getData())
 
 
 
@@ -15,12 +32,19 @@
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 
 // 3a. Write a function that returns every other item as one string. Expected output: "streetlamp teeth nephew database"
+let mkString = (arr) => {
+    let newArr = []
+        for(let i = 0; i < arr.length; i += 2)
+            newArr.push(arr[i])
+            return newArr.join(' ')
+}
+console.log(mkString(randomNouns))
 
-
-
-// 3b. Create an arrow function that takes in the variable and returns an array with the number of letters in each string. Expected output: [10, 6, 5, 10, 6, 11, 8]
-
-
+// // 3b. Create an arrow function that takes in the variable and returns an array with the number of letters in each string. Expected output: [10, 6, 5, 10, 6, 11, 8]
+let indexLength = (str) => {
+    return str.map(value => value.length)
+}
+console.log(indexLength(randomNouns))
 
 // 3c. Stretch: Create a function called capitalizer that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
 
@@ -33,11 +57,23 @@ var testString2 = "academy"
 var testString3 = "sandiego"
 
 // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
-
-
+let alphabetSoup = (strings) => {
+    let newArr = strings.split('')
+    newArr.sort()
+    return newArr.join('')
+}
+console.log(alphabetSoup(testString1))
+console.log(alphabetSoup(testString2))
+console.log(alphabetSoup(testString3))
 
 // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
-
+let alphabetSoup1 = (string1, string2, string3) => {
+    let letters = string1 + string2 + string3
+    let newArr1 = letters.split('')
+    newArr1.sort()
+    return newArr1.join('')
+}
+console.log(alphabetSoup1(testString1, testString2, testString3))
 
 
 // -------------------------------------------------
@@ -46,7 +82,12 @@ var amounts = [9, 1, 8, 16, 5, 1, 42]
 var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguins", "llamas"]
 
 // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
-
+let combined = (str1, str2) => {
+  
+    // let newArr2 = both.join('')
+    return `${str1[0]} ${str2[0]} ${str1[1]} ${str2[1]} ${str1[2]} ${str2[2]} ${str1[3]} ${str2[3]} ${str1[4]} ${str2[4]} ${str1[5]} ${str2[5]} ${str1[6]} ${str2[6]}`
+}
+console.log(combined(amounts, animals))
 
 
 // 5b. Stretch: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
--- js-foundations.md
+++ js-foundations.md
@@ -4,23 +4,30 @@
 ### From memory, answer the following:
 
 1. What's the difference between =, ==, and === in JavaScript?
-
+= is for defining variables
+== is loosely equals (ex. '1' = 1)
+=== is exactly equals (ex. 1 = 1)
 
 
 2. What is the difference between an array's index and length?
-
+index starts at 0 and calculates the place of a value
+length starts at 1 and determines how many places the array takes up
 
 
 3. What is a function's declaration, argument, and call?
-
+declaration - is naming a function
+argument - what you pass through the function
+call - is what actually runs the function
 
 
 4. What are the three main steps in saving work to github?
-
+git add .
+git commit -m 'filename'
+git push
 
 
 5. What is an object?
-
+an object is an array of attributes
 
 
 ### Answer the following questions. First, without external resources. Challenge yourself to answer from memory. Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn.
@@ -36,59 +43,60 @@ logIt()
 
 
 1a. Look at this Javascript function and try to predict what the console show.
-
+it will show inside
 
 
 1b. Test the function. Explain why the function returned what it did.
 
-  Your answer:
+  Your answer:it returned undefined because there was nothing passed in
 
-  Researched answer:
+  Researched answer: because nothing was passed in
 
 
 2. What is JSON? How does it relate to javascript objects?
 
-  Your answer:
+  Your answer: javascript object nototation
 
-  Researched answer:
+  Researched answer: JavaScript Object Notation is an open-standard file format that uses human-readable text to transmit data objects consisting of attribute–value pairs and array data types
 
 
 3. What does CRUD stand for?
 
-  Your answer:
+  Your answer: create, read, update, delete/destroy
 
-  Researched answer:
+  Researched answer: create, read, update, delete/destroy
 
 
 4. What is a higher-order function?
 
-  Your answer:
+  Your answer: is a function that holds other functions
 
-  Researched answer:
+  Researched answer: A higher-order function is a function that can take another function as an argument, or that returns a function as a result
 
 
 5. Stretch: What is a closure, what is it good for and how do you recognize one?
 
-  Your answer:
+  Your answer: i am not sure
 
-  Researched answer:
+  Researched answer:  closure gives you access to an outer function's scope from an inner function
 
 
 6. Stretch: What is an API?
 
-  Your answer:
+  Your answer: usually a json file, basically computer code with no ux
 
-  Researched answer:
+  Researched answer: in application programming interface is a set of subroutine definitions, communication protocols, and tools for building software
 
 
 ### Additional Feedback Questions.
 
 1. Do you have a suggestion for a Check In question?
-
+Favorite musical artist or band, then elaborate on what the genre is like.
 
 
 2. What was your favorite topic this week?
-
+react
 
 
 3. What was your "A-ha!" moment this week?
+how to make inheritance work
